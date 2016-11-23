15e3b77f381afd876554409f19fe05c65f340f39
Tomasz Kornuta <tkornut@us.ibm.com>
2016-11-23 13:29:04 -0800

    Travis test 10

--- src/test/matrixSpeedTest.cpp
+++ src/test/matrixSpeedTest.cpp
@@ -78,6 +78,7 @@ mic::types2::Matrix<float> BLAS_mmultilpy(mic::types2::Matrix<float>& __restrict
 #endif /* USE_BLAS */
 
 
+
 /*!
  * \brief Program for testing multiplication of matrices with/without OpenBLAS.
  * \author tkornuta
