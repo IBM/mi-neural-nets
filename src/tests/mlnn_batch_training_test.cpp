327a86bc8a867355898661851df0565fcbbba8e7
Tomasz Kornuta <tkornut@us.ibm.com>
2017-04-13 00:26:02 -0700

    mlnn test apps working, cmake so linking fix

--- src/tests/mlnn_batch_training_test.cpp
+++ src/tests/mlnn_batch_training_test.cpp
@@ -28,48 +28,49 @@ int main() {
 	LOGGER->addOutput(new ConsoleOutput());
 
 	// Generate a dataset.
-	size_t dataset_size = 10;
+	size_t dataset_size = 15;
+	size_t classes = 5;
 	Batch<MatrixXf, MatrixXf> dataset;
 	for(size_t i=0; i< dataset_size; i++) {
 		// Generate "pose".
-		MatrixXfPtr pose (new MatrixXf(10, 1));
+		MatrixXfPtr pose (new MatrixXf(dataset_size, 1));
 		pose->setZero();
 		(*pose)(i,0)=1;
 		dataset.data().push_back(pose);
 
 		// Generate desired target.
-		MatrixXfPtr target (new MatrixXf(4, 1));
+		MatrixXfPtr target (new MatrixXf(classes, 1));
 		target->setZero();
-		(*target)(i%4,0)= (i%4);
+		(*target)(i%classes,0)= 1;//(i%4);
 		dataset.labels().push_back(target);
 
 		// Add index.
 		dataset.indices().push_back(i);
 	}//: for
-	dataset.setBatchSize(5);
 
 	// Neural net.
 	MultiLayerNeuralNetwork<float, mic::neural_nets::loss::CrossEntropyLoss<float> > nn("simple_linear_network");
-	nn.pushLayer(new Linear<float>(dataset_size, 4, "Linear1"));
-	nn.pushLayer(new ReLU<float>(4, "ReLU1"));
-	nn.pushLayer(new Softmax<float>(4, "Softmax"));
+	nn.pushLayer(new Linear<float>(dataset_size, 15, "Linear1"));
+	nn.pushLayer(new ReLU<float>(15, "ReLU1"));
+	nn.pushLayer(new Linear<float>(15, classes, "Linear2"));
+	nn.pushLayer(new Softmax<float>(classes, "Softmax"));
 
 	// Initialize the encoders.
 	mic::encoders::MatrixXfMatrixXfEncoder data_encoder(dataset_size, 1);
-	mic::encoders::MatrixXfMatrixXfEncoder label_encoder(4, 1);
+	mic::encoders::MatrixXfMatrixXfEncoder label_encoder(classes, 1);
 
 	// Training.
 	size_t iteration = 0;
-	while (iteration < 100000) {
+	while (iteration < 10000) {
 		Batch <MatrixXf, MatrixXf> batch = dataset.getRandomBatch();
-		//std::cout << "[" << iteration++ << "]: sample (" << sample.index() << "): "<< sample.data()->transpose() << "->" << sample.label()->transpose() << std::endl;
+		//std::cout << "[" << iteration << "]: sample (" << sample.index() << "): "<< sample.data()->transpose() << "->" << sample.label()->transpose() << std::endl;
 
 		MatrixXfPtr encoded_batch, encoded_targets;
 		encoded_batch  = data_encoder.encodeBatch(batch.data());
 		encoded_targets  = label_encoder.encodeBatch(batch.labels());
 
 		// Train network with batch.
-		float loss = nn.train (encoded_batch, encoded_targets, 0.005);
+		float loss = nn.train (encoded_batch, encoded_targets, 0.1);
 
 		if (iteration % 1000 == 0){
 			std::cout<<"[" << iteration << "]: Loss        : " << loss << std::endl;
@@ -84,8 +85,9 @@ int main() {
 
 	// Test network
 	iteration = 0;
-	while (iteration < 10) {
-		Sample <MatrixXf, MatrixXf> sample = dataset.getRandomSample();
+	dataset.setNextSampleIndex(0);
+	while (iteration < dataset_size) {
+		Sample <MatrixXf, MatrixXf> sample = dataset.getNextSample();
 		std::cout << "[" << iteration++ << "]: sample (" << sample.index() << "): "<< sample.data()->transpose() << "->" << sample.label()->transpose() << std::endl;
 
 		float loss = nn.test(sample.data(), sample.label());
--- src/tests/mlnn_sample_training_test.cpp
+++ src/tests/mlnn_sample_training_test.cpp
@@ -26,7 +26,7 @@ int main() {
 	LOGGER->addOutput(new ConsoleOutput());
 
 	// Generate a dataset.
-	size_t dataset_size = 10;
+	size_t dataset_size = 15;
 	size_t classes = 5;
 	Batch<MatrixXf, MatrixXf> dataset;
 	for(size_t i=0; i< dataset_size; i++) {
@@ -57,25 +57,25 @@ int main() {
 	size_t iteration = 0;
 	while (iteration < 10000) {
 		Sample <MatrixXf, MatrixXf> sample = dataset.getRandomSample();
-		std::cout << "[" << iteration++ << "]: sample (" << sample.index() << "): "<< sample.data()->transpose() << "->" << sample.label()->transpose() << std::endl;
+		//std::cout << "[" << iteration << "]: sample (" << sample.index() << "): "<< sample.data()->transpose() << "->" << sample.label()->transpose() << std::endl;
 
-		float loss = nn.train(sample.data(), sample.label(), 0.01);
+		float loss = nn.train(sample.data(), sample.label(), 0.1);
 
 
-		//if (iteration % 1000 == 0)
+		if (iteration % 1000 == 0)
 			std::cout<<"[" << iteration << "]: Loss        : " << loss << std::endl;
 
 		// Compare results.
-		MatrixXf predictions = (*nn.getPredictions());
+/*		MatrixXf predictions = (*nn.getPredictions());
 		std::cout<<"Targets     : " << sample.label()->transpose() << std::endl;
-		std::cout<<"Predictions : " << predictions.transpose() << std::endl << std::endl;
-
+		std::cout<<"Predictions : " << predictions.transpose() << std::endl << std::endl;*/
+		iteration++;
 	}//: while
 
 	// Test network
 	iteration = 0;
 	dataset.setNextSampleIndex(0);
-	while (iteration < 10) {
+	while (iteration < dataset_size) {
 		Sample <MatrixXf, MatrixXf> sample = dataset.getNextSample();
 		std::cout << "[" << iteration++ << "]: sample (" << sample.index() << "): "<< sample.data()->transpose() << "->" << sample.label()->transpose() << std::endl;
 
